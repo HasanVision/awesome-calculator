72e5bdd975ee8e596b241c2317e107d152617e8f
HasanVision <hasan.visionx@gmail.com>
2024-03-18 12:06:07 -0400

    xcode github test

--- awesome calculator/ContentView.swift
+++ awesome calculator/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Awesome Calculator")
+            Text("Awesome Calculator for IOS")
         }
         .padding()
     }
